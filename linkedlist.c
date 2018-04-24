0037f9946da53ca21dd38d8e97e58e1ac17071e8
Chen Yitong <cheny2@eduroam-175-42.dyn.carleton.edu>
2018-04-24 18:04:13 -0400

    push link

--- linkedlist.c
+++ linkedlist.c
@@ -12,26 +12,71 @@
 
 
 #include "linkedlist.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /*
  * Create an empty list (a new Value object of type NULL_TYPE).
  */
 Value *makeNull(){
-    
+    Value *emptyList = malloc(sizeof(Value));
+    emptyList->type = NULL_TYPE;
+    return emptyList;
 }
 
 /*
  * Create a nonempty list (a new Value object of type CONS_TYPE).
  */
 Value *cons(Value *car, Value *cdr){
-    
+    struct ConsCell newCell;
+    newCell.car = car;
+    newCell.cdr = cdr;
+    Value *newList = malloc(sizeof(Value));
+    newList->type = CONS_TYPE;
+    newList->c = newCell;
+    return newList;
 }
 
 /*
  * Print a representation of the contents of a linked list.
  */
 void display(Value *list){
-    
+    Value *cur;
+    for (cur = list; cur; cur = cur->c.cdr) {
+        switch (cur->c.car->type) {
+            case INT_TYPE:
+                printf("%i => ", cur->c.car->i);
+                break;
+            case DOUBLE_TYPE:
+                printf("%f => ", cur->c.car->d);
+                break;
+            case STR_TYPE:
+                printf("%s => ", cur->c.car->s);
+                break;
+            case CONS_TYPE:
+                display(cur->c.car);
+                break;
+            case NULL_TYPE:
+                return;
+        }
+        printf("%i", cur->c.car->type);
+    }
+//    Value *cur = list;
+//    printf("cur type: %i\n", cur->type);
+//    if (cur->type != NULL_TYPE){
+//    cur = cur->c.cdr;}
+////    printf("cur type: %d\n", cur->c.cdr)
+//    int j = 0;
+//  //  for (int i = 0; i < 2; i ++) {
+////        cur = cur->c.cdr;
+//        printf("%s\n", cur->c.car->s);
+//   // }
+//    printf("\n");
+//    Value *cur;
+//    cur = list;
+//    printf("cdr: %d\n", cur->c.cdr->type);
+//    cur = cur->c.cdr;
+//    printf("cdr: %d\n", cur->c.cdr->type);
 }
 
 /*
