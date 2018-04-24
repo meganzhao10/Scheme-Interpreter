8009e30b44c61b47bb1dea3d1ab2198a3ac7b2fb
Yingying Wang <35354872+yingyingww@users.noreply.github.com>
2018-04-24 17:51:57 -0500

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -103,7 +103,7 @@ Value *reverse(Value *list){
  *      be after we've got an easier way of managing memory.
 */
 void cleanup(Value *list){
-    assert(list->type != NULL_TYPE);
+    assert(list != NULL);
     Value *next;
     //always free() what we malloc()
     for (Value *cur = list->c.car; cur; cur = next){
