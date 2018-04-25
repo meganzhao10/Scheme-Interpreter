2c56721eaf78c06de9ba25e4fa26362fa429db52
Chen Yitong <cheny2@eduroam-175-42.dyn.carleton.edu>
2018-04-25 14:34:54 -0400

    remove local version of car, cdr, display and cleanup

--- linkedlist.c
+++ linkedlist.c
@@ -38,67 +38,6 @@ Value *cons(Value *car, Value *cdr) {
     return newValue;
 }
 
-
-/*
- * Print a representation of the contents of a linked list.
- */
-void display(Value *list) {
-    switch (list->type) {
-        case INT_TYPE: 
-            printf("%i", list->i);
-            break;
-        case DOUBLE_TYPE:
-            printf("%f", list->d);
-            break;
-        case STR_TYPE:
-            printf("%s", list->s);
-            break;
-        case NULL_TYPE:
-            printf("()");
-            break;
-        case CONS_TYPE:
-            printf("(");
-            for (Value *cur = list; cur->type != NULL_TYPE; cur = cur->c.cdr) {
-                switch (cur->c.car->type) {
-                    case INT_TYPE:
-                        printf("%i ", cur->c.car->i);
-                        break;
-                    case DOUBLE_TYPE:
-                        printf("%f ", cur->c.car->d);
-                        break;
-                    case STR_TYPE:
-                        printf("%s ", cur->c.car->s);
-                        break;
-                    case CONS_TYPE:
-                        display(cur->c.car);
-                        break;
-                    case NULL_TYPE:
-                        printf("");
-                } 
-            }
-            printf(")");
-            break;
-    }
-    printf("\n");
-}
-
-/*
- * Get the car value of a given list.
- * (Uses assertions to ensure that this is a legitimate operation.)
- */
-Value *car(Value *list) {
-    return list->c.car;
-}
-
-/*
- * Get the cdr value of a given list.
- * (Uses assertions to ensure that this is a legitimate operation.)
- */
-Value *cdr(Value *list) {
-    return list->c.cdr;
-}
-
-
 /*
  * Create a new linked list whose entries correspond to the given list's
  * entries, but in reverse order.  The resulting list is a deep copy of the
@@ -136,23 +75,4 @@ Value *reverse(Value *list) {
         reversed = cons(new_value, reversed);
     }
     return reversed;
-}
-
-void cleanup(Value *list){
-    
-    Value *next;
-    //printf("first free:\n");
-    
-//    free(list->c.car);
-    
-    //always free() what we malloc()
-    for (Value *cur = list; cur->type == CONS_TYPE; cur = next){
-        printf("run here\n");
-        //printf("cur:\n");
-//        //display(cur);
-        next = cur->c.cdr;
-//        //printf("next:\n");
-//        //display(next);
-        free(cur->c.car);
-    }
 }
