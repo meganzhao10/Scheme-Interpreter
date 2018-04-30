496b5415bc0fc2df55611852e9a734f9a0fcf483
meganzhao <zhaoxiazhao.megan@gmail.com>
2018-04-30 13:04:43 -0500

    Add makeNull function to talloc.c

--- talloc.c
+++ talloc.c
@@ -1,12 +1,31 @@
-#include <stdlib.h>
-#include "value.h"
+/* This code implements a garbage collector to manage memory
+ * usage throughout the interpreter project. 
+ * 
+ * Authors: Yitong Chen, Yingying Wang, Megan Zhao
+ */
+
 #include <assert.h>
 #include <stdio.h>
 #include "talloc.h"
 
-
-
+// The golbal static variable
 static Value *head;
+
+/*
+ * Create an empty list (a new Value object of type NULL_TYPE).
+ *
+ * Returns a pointer to an empty list.
+ * If memory allocation fails, returns a null pointer.
+ */
+Value *talloc_makeNull() {
+    Value *nullList = malloc(sizeof(Value));
+    if (!nullList) {
+        printf("Out of memory!\n");
+        return nullList;
+    }
+    nullList->type = NULL_TYPE;
+    return nullList;
+}
 
 /*
  * Create a nonempty list (a new Value object of type CONS_TYPE).
@@ -39,9 +58,17 @@ Value *talloc_cons(Value *car, Value *cdr) {
  * modify the linked list to use talloc instead of malloc.)
  */
 void *talloc(size_t size){
+	if (head == NULL){
+		head = talloc_makeNull();
+		if (!head){
+			printf("Out of memory!\n");
+			return head;
+		}
+	}
 	Value *newHead = malloc(sizeof(Value));
 	if (!newHead){
 		printf("Out of memory!\n");
+		return newHead;
 	}
 	newHead->type = PTR_TYPE;
 	newHead->p = malloc(size);
@@ -59,13 +86,14 @@ void *talloc(size_t size){
 void tfree(){
 	Value *cur = head;
 	Value *next;
-	while (cur != NULL && cur->type == CONS_TYPE){
+	while (cur->type != NULL_TYPE){
 		next = cur->c.cdr;
 		free(cur->c.car->p);
 		free(cur->c.car);
 		free(cur);
 		cur = next;
 	} 
+	free(next);
 	head = NULL;
 }
 
