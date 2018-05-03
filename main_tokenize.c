ec5780fb7dbfd3c9254f558c313b11dbbd908b66
Chen Yitong <cheny2@eduroam-175-42.dyn.carleton.edu>
2018-05-03 17:09:13 -0400

    commit the tokenizer starter file

---
+++ main_tokenize.c
@@ -0,0 +1,13 @@
+/* 
+ * Authors: Yitong Chen, Yingying Wang, Megan Zhao
+ */
+
+#include <stdlib.h>
+#include "tokenizer.h"
+
+int main(void) {
+        Value *list = tokenize();
+        displayTokens(list);
+        tfree();
+        return 0;
+}
---
+++ tokenizer.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "value.h"
+
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+/* The tokenize function should read stdin (the standard input 
+ * stream in C) in its entirety and return a linked list consisting 
+ * of all tokens found.
+ */
+Value *tokenize();
+
+/* The displayTokens function takes a linked list of tokens as 
+ * input, and displays those tokens, one per line, with each 
+ * token's type.
+ */
+void displayTokens(Value *list);
+
+#endif
