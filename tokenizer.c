52cc5eef4aed17fba5c1698664465594a4e45cdc
Yitong Chen <31914017+yitongc19@users.noreply.github.com>
2018-05-07 22:44:27 -0400

    Update tokenizer.c
    Removed duplicates

--- tokenizer.c
+++ tokenizer.c
@@ -13,32 +13,6 @@
 #include "talloc.h"
 #include "value.h"
 
-bool isLetter(char ch){
-    if (ch == 'a' || ch == 'b' || ch == 'c' || ch == 'd' || ch == 'e' ||
-        ch == 'g' || ch == 'h' || ch == 'i' || ch == 'j' || ch == 'k' ||
-        ch == 'l' || ch == 'm' || ch == 'n' || ch == 'o' || ch == 'p' ||
-        ch == 'q' || ch == 'r' || ch == 's' || ch == 't' || ch == 'f' ||
-        ch == 'v' || ch == 'w' || ch == 'x' || ch == 'y' || ch == 'z' ||
-        ch == 'A' || ch == 'B' || ch == 'C' || ch == 'D' || ch == 'E' ||
-        ch == 'G' || ch == 'H' || ch == 'I' || ch == 'J' || ch == 'K' ||
-        ch == 'L' || ch == 'M' || ch == 'N' || ch == 'O' || ch == 'P' ||
-        ch == 'Q' || ch == 'R' || ch == 'S' || ch == 'T' || ch == 'F' ||
-        ch == 'V' || ch == 'W' || ch == 'X' || ch == 'Y' || ch == 'Z') {
-        return true;
-    }
-    return false;
-}
-
-bool isDigit(char ch){
-    if (ch == '0' || ch == '1' || ch == '2' || ch == '3' || ch == '4' ||
-        ch == '5' || ch == '6' || ch == '7' || ch == '8' || ch == '9') {
-        return true;
-    }
-    return false;
-}
-
-
-
 /* 
  * Check whether the input character is a delimiter.
  *
